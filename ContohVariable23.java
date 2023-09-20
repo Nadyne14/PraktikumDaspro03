5e00dd02b4bf8f46215c28c3ccf2586f0a79ac49
Nadyne14 <nadynerosalia1234@gmail.com>
2023-09-20 14:52:57 +0700

    commit 3

---
+++ ContohVariable23.java
@@ -0,0 +1,19 @@
+public class ContohVariable23{
+    public static void main(String[] args){
+
+    String salahSatuHobiSayaAdalah = "Bermain petak umpet";
+    boolean isiPandai = true;
+    char jenisKelamin = 'P';
+    byte _umurSayaSekarang = 19;
+    double $ipk = 4.00, tinggi = 1.65;
+
+    System.out.println(salahSatuHobiSayaAdalah);
+    System.out.println("Apakah pandai? " + isiPandai);
+    System.out.println("Jenis Kelamin: " +jenisKelamin);
+    System.out.println("IPK: " + $ipk);
+    System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+
+
+    }
+
+}
